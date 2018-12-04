07b3c3b1ea1eb733832eb3e69314d61115d8ff05
Andrey Shakhmin <andrey.shakhmin@gmail.com>
2018-12-04 12:55:35 +0800

    added native module implementation

---
+++ obj-c/bin/bin/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  bin
+//
+//  Created by Andrey on 12/4/18.
+//  Copyright © 2018 Turbobabr. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
